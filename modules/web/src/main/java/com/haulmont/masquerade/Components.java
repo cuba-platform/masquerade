82759e995631d7ed3ddd9ef36e4dd8bc57be3d29
Yuriy Artamonov <artamonov@haulmont.com>
2017-04-29 15:12:18 +0400

    Fix LookupField import

--- modules/web/src/main/java/com/haulmont/masquerade/Components.java
+++ modules/web/src/main/java/com/haulmont/masquerade/Components.java
@@ -31,6 +31,7 @@ public class Components {
         components.put(Button.class, ButtonImpl::new);
         components.put(CheckBox.class, CheckBoxImpl::new);
         components.put(AppMenu.class, AppMenuImpl::new);
+        components.put(LookupField.class, LookupFieldImpl::new);
 
         String cubaVersion = System.getProperty(CUBA_VERSION_SYSTEM_PROPERTY);
         if (cubaVersion != null && "5.x".equals(cubaVersion)) {
