f40caf0524586b5e44d9e769ff5b4523740dc89d
Mikhail Tokarenko <tokarenko@haulmont.com>
2017-11-19 18:58:59 +0400

    implemented methods getValue() and setValue() for MaskField

--- modules/web/src/main/java/com/haulmont/masquerade/components/impl/fresh/MaskedFieldImpl.java
+++ modules/web/src/main/java/com/haulmont/masquerade/components/impl/fresh/MaskedFieldImpl.java
@@ -2,21 +2,36 @@ package com.haulmont.masquerade.components.impl.fresh;
 
 import com.haulmont.masquerade.components.MaskedField;
 import com.haulmont.masquerade.components.impl.AbstractComponent;
-import org.apache.commons.lang3.NotImplementedException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.enabled;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.haulmont.masquerade.Conditions.editable;
 
 public class MaskedFieldImpl extends AbstractComponent<MaskedField> implements MaskedField {
     public MaskedFieldImpl(By by) {
         super(by);
     }
 
+
     @Override
     public MaskedField setValue(String value) {
-        throw new NotImplementedException("This UI component still in development");
+        $(impl)
+                .shouldBe(visible)
+                .shouldBe(enabled)
+                .shouldBe(editable)
+                .click();
+        $(impl).sendKeys(Keys.HOME, value);
+        return this;
     }
 
     @Override
     public String getValue() {
-        throw new NotImplementedException("This UI component still in development");
+        return $(impl)
+                .shouldBe(visible)
+                .shouldBe(enabled)
+                .getValue();
     }
 }
